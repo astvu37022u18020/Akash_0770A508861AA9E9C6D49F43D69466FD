b4195b28057f64d245875728a9fd55e1912e1c66
astvu37022u18020 <145248074+astvu37022u18020@users.noreply.github.com>
2023-09-21 09:51:23 -0700

    Add files via upload

---
+++ Unit2_Challange2/main.py
@@ -0,0 +1,17 @@
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+        
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting\n")
+        
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling")
+
+b = Batsman()
+b1 = Bowler()
+b.play()
+b1.play()
+
